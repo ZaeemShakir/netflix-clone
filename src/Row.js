bb116c67d2053aa12210c90ed30c9cc37b1537c4
Your Name <you@example.com>
2021-08-26 03:10:59 +0500

    Fourth Commit

--- src/Row.js
+++ src/Row.js
@@ -1,18 +1,36 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "./axios";
+import "./Row.css";
+const base_url = "https://image.tmdb.org/t/p/original/";
+
 function Row({ title, fetchUrl }) {
-  const [movies, setmovies] = useState([]);
+  const [movies, setMovies] = useState([]);
+
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
-      setmovies(request.data.result);
+      setMovies(request.data.results);
+      console.log(request);
       return request;
     }
     fetchData();
   }, [fetchUrl]);
+  console.table(movies);
+
   return (
-    <div>
-      <h2>{title}</h2>
+    <div className="row">
+      <h1>{title}</h1>
+      <div className="posters">
+        {movies.map((movie) => {
+          return (
+            <img
+              className="row_posters"
+              src={`${base_url}${movie.poster_path}`}
+              alt={movie.name}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
