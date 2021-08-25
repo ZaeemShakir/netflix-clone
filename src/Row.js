37096af66b069a25baca234d33eb92d0fda6cb0b
Your Name <you@example.com>
2021-08-26 01:45:23 +0500

    second commit

--- src/Row.js
+++ src/Row.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
+import axios from "./axios";
 function Row({ title }) {
   const [movies, setmovies] = useState([]);
+  useEffect(() => {}, []);
   return (
     <div>
       <h2>{title}</h2>
