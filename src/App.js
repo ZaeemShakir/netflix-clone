df26b6a13f6f325d0f97373b053868ce1592a4fc
Your Name <you@example.com>
2021-08-26 03:30:13 +0500

    Third Commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
       <div>
         <Row title="Netflix Original" fetchUrl={request.fetchNetflixOrignals} />
         <Row title="Trendings" fetchUrl={request.fetchTrending} />
+        <Row title="Romance" fetchUrl={request.fetchRomanceMovies} />
+        <Row title="Top Rated" fetchUrl={request.fetchTopRated} />
+        <Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies} />
+        <Row title="Action Movies" fetchUrl={request.fetchActionMovies} />
+        <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies} />
+        <Row title="Documentaries" fetchUrl={request.fetchDocumentaries} />
       </div>
     </div>
   );
