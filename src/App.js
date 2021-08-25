f53e87c724c80dc69dd79707925e23c00f72004f
Your Name <you@example.com>
2021-08-26 01:37:15 +0500

    third commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
-
+import Row from "./Row";
+import React from "react";
+import request from "./request";
 function App() {
   return (
     <div className="App">
-      <div></div>
+      <div>
+        <Row title="Netflix Orignal" fetchURL={request.fetchNetflixOrignals} />
+        <Row title="Trendings" fetchURL={request.fetchTrending} />
+      </div>
     </div>
   );
 }
